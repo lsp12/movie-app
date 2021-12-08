60ca61a3d1ee2174b96223fa68392d15a1d6a765
lsp12 <jonathankenny852@gmail.com>
2021-12-08 10:38:02 -0500

    update

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-// eslint-disable-next-line prefer-const
-let port = process.env.PORT || 4000;
 async function bootstrap() {
+  const port = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
 }
